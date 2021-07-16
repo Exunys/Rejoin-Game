5649634fada1a64e31a8bbebde96c225e64ce6bb
Exunys <76539058+Exunys@users.noreply.github.com>
2021-07-16 15:37:08 +0200

    Script

---
+++ Rejoin Game.lua
@@ -0,0 +1,15 @@
+local TeleportService = game:GetService("TeleportService")
+local Players = game:GetService("Players")
+local LocalPlayer = Players.LocalPlayer
+
+local Rejoin = coroutine.create(function()
+    local Success, ErrorMessage = pcall(function()
+        TeleportService:Teleport(game.PlaceId, LocalPlayer)
+    end)
+
+    if ErrorMessage and not Success then
+        warn(ErrorMessage)
+    end
+end)
+
+coroutine.resume(Rejoin)
